354cb2c599e8283fe24fecd0fa301dd9c772fb1f
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2021-10-23 17:07:27 +0000

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 app.get('/species', async (req, res) => {
-	const rangeSpecies = `Sheet1!A2:I`;
+	const rangeSpecies = `Sheet1!A2:J`;
 
 	const response = await Promise.all([
 		sheets.spreadsheets.values.get({
@@ -27,21 +27,22 @@ app.get('/species', async (req, res) => {
 	const speciesResponse = response[0].data.values;
 	var speciesData = speciesResponse.map(function(x) { 
 	  return { 
-		name: x[0], 
-		scientificName: x[1], 
-		conservationStatus: x[2],
-		population: x[3],
-		height: x[4],
-		weight: x[5],
-		length: x[6], 
-		places: x[7],
-		description: x[8] 
+		_id: x[0],
+		name: x[1], 
+		scientificName: x[2], 
+		conservationStatus: x[3],
+		population: x[4],
+		height: x[5],
+		weight: x[6],
+		length: x[7], 
+		places: x[8],
+		description: x[9] 
 	  }; 
 	});
-	for (let i = 0; i < speciesData.length; i++) {
-		if (speciesData[i].conservationStatus.toLowerCase() === 'vulnerable')
-			console.log(speciesData[i].name)
-	}
+	// for (let i = 0; i < speciesData.length; i++) {
+	// 	if (speciesData[i].conservationStatus.toLowerCase() === 'vulnerable')
+	// 		console.log(speciesData[i].name)
+	// }
 	res.send(speciesData);
 });
 
